a9ae8701b8c4bed75145d499bb4ae3ca4ab9593f
vinod.beli@gmail.com <vinodbeli>
2018-10-16 01:20:14 +0000

    File generated by Skaffolder - user: vinod.beli@gmail.com

---
+++ client/app/pages/home/home.component.ts
@@ -0,0 +1,30 @@
+// Import Libraries
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+
+// Import Services
+
+// START - USED SERVICES
+
+// END - USED SERVICES
+
+// START - REQUIRED RESOURCES
+
+// END - REQUIRED RESOURCES
+
+/**
+ * Home Component
+ */
+@Component({
+    selector: 'home',
+    templateUrl : './home.component.html',
+    styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent {
+    
+    constructor(
+        private location: Location) { 
+        
+    }
+}
